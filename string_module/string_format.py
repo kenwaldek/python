3ea73009f3134e18d2e969ac9b47604fe95982d2
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-24 15:48:56 +0100

    update
    update

--- string_module/string_format.py
+++ string_module/string_format.py
@@ -9,9 +9,17 @@
 #
 ###############################################################
 print ('{:10}'.format('test'))
-
+###
 name = "kenny"
 print ("my name is {:1} and it works".format(name))
-
+###
 print ('{:d}'.format(42)) # nummers = :d van decimaal
+###
 print ("{:f}".format(3.141592653589793)) # float na de comma dus PI
+###
+a = "dit"
+b = "zijn"
+c = "variablen"
+print('{0}, {1}, {2}'.format('a', 'b', 'c')) # dit zijn strings
+print('{0}, {1}, {2}'.format(a, b, c)) # dit zijn de variablen
+###
