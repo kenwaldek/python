bee960bc177629cfc04284a79d0d398e3272fe5a
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-24 15:13:33 +0100

    example of format command
    sample of format command

---
+++ string_module/string_format.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+#  -*- coding:utf-8 -*-
+###############################################################
+# © kenwaldek
+#
+# Title: .format                Version:
+# Date:  24-12-16               Language: python3
+# Description: testing .format string format
+#
+###############################################################
+print ('{:10}'.format('test'))
+
+name = "kenny"
+print ("my name is {:1} and it works".format(name))
+
+print ('{:d}'.format(42)) # nummers = :d van decimaal
+print ("{:f}".format(3.141592653589793)) # float na de comma dus PI
