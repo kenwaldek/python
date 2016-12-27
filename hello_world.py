a328fe38f37ec985068bc752c63c56fe4c9c68f2
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-27 16:54:27 +0100

    updated modules example

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
+#! /usr/bin/env python3
+#  -*- coding:utf-8 -*-
+###############################################################
+# © kenwaldek
+#
+# Title: hello world              Version: 1.0
+# Date: 27-12-16                  Language: python3
+# Description: just a hello world the beginning of everything
+#
+###############################################################
 print("hello world")
+
