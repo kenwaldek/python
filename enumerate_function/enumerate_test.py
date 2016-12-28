7c3768bbe51d0f5483037134308cab290e4d8327
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-28 19:08:50 +0100

    update

---
+++ enumerate_function/enumerate_test.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+#  -*- coding:utf-8 -*-
+###############################################################
+# © kenwaldek
+#
+# Title: enumerate function               Version: 1.0
+# Date: 28-12-16                          Language: python3
+# Description: return an tuplle containing the count
+# and the list variables
+###############################################################
+
+example = ['left', 'right', 'up', 'down']
+
+
+for i, j in enumerate(example):  #juiste manier om genummerde lijst te maken
+    print(i, j)
+
+# for i in range(len(example)):  # dit is geen juiste manier
+#     print(i, example(i))       # dit is geen juiste manier
+print('###')
+new_dict = dict(enumerate(example))  # print een dictionary
+
+print(new_dict)
+print('###')
+[print(i, j) for i, j in enumerate(new_dict)]
+
+
