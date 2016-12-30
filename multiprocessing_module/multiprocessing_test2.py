d76300fdde5f86ccf800d3f9c16106906df98daa
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-30 17:30:43 +0100

    update

---
+++ multiprocessing_module/multiprocessing_test2.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+#  -*- coding:utf-8 -*-
+###############################################################
+# © kenwaldek                             GPL-license
+#
+# Title: multiprocessing 2                Version: 1.0
+# Date: 30-12-16                          Language: python3
+# Description: multiprocessing dus  met meerdere cores te samen
+#
+###############################################################
+
+from multiprocessing import Pool
+
+def job(num):
+    return num * 2
+
+if __name__ == '__main__':
+    p = Pool(processes=20)
+    data = p.map(job, range(20))
+    p.close()
+    print(data)
+
+
---
+++ pyqt_module/pyqt_1.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+#  -*- coding:utf-8 -*-
+###############################################################
+# © kenwaldek                         GPL-license
+#
+# Title: pyqt test                    Version: 1.0
+# Date: 30-12-16                      Language: python3
+# Description: example code for qt gui
+#
+###############################################################
+#todo iets niet in orde nazoeken iets met 'SIP'
+from PyQt5 import QtGui
+import sys
+
+
+app = QtGui.QApplication()
+
+window = QtGui.QWidget()
+
+window.show()
+
+
+
