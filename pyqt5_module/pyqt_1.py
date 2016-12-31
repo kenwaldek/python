68e2010b970cfa5112aa1453dc31c7de3dba2030
vanbillemont kenny <kenwaldek@gmail.com>
2016-12-31 17:07:20 +0100

    update

--- pyqt5_module/pyqt_1.py
+++ pyqt5_module/pyqt_1.py
@@ -5,21 +5,32 @@
 #
 # Title: PyQt5                       Version: 1.0
 # Date:30-12-16                      Language: python3
-# Description: qt5 gui
+# Description: qt5 gui leeg scherm
 #
 ###############################################################
 # do something
-
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog
-from ui_imagedialog import Ui_ImageDialog
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-app = QApplication(sys.argv)
-window = QDialog()
-ui = Ui_ImageDialog()
-ui.setupUi(window)
 
-window.show()
-sys.exit(app.exec_())
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 simple window - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
 
-#todo voor één of andere reden wwerkt het nog niet
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
